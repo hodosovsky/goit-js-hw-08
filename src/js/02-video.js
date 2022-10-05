b02eadb3f0f5e14c015a815775386fc132ea1120
Mike <hodosovsky@gmail.com>
2022-10-05 18:51:15 +0300

    Update video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,14 +1,46 @@
+// import Player from '@vimeo/player';
 import Player from '@vimeo/player';
 
-const player = new Player('handstick', {
-  id: 19231868,
-  width: 640,
-});
+// const player = new Player('handstick', {
+//   id: 19231868,
 
-player.on('play', function () {
-  console.log('played the video!');
-});
+//   width: 640,
+// });
 
+// player.on('play', function () {
+//   console.log('played the video!');
+// });
+
+// const onPlay = function (data) {
+//   // data is an object containing properties specific to that event
+// };
+
+// player.on('play', onPlay);
 const iframe = document.querySelector('iframe');
-const iframePlayer = new Vimeo.Player(iframe);
-console.log(iframePlayer);
+const player = new Player(iframe);
+
+player.on('play', function (event) {
+  console.log('played the video!');
+  console.log(player);
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+player.setVolume(0.5);
+
+console.log(player);
+
+const onPlay = function (data) {
+  player.setCurrentTime(time);
+};
+
+player.on('play', onPlay);
+let time = 0;
+const timeupdate = function (data) {
+  time = data.seconds;
+  console.log(time);
+};
+
+player.on('timeupdate', timeupdate);
