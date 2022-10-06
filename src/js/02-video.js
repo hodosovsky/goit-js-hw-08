f241f94e9aa6ae85a9d590145afb33cbb1c57254
Mike <hodosovsky@gmail.com>
2022-10-06 15:36:10 +0300

    submitForm - done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,46 +1,55 @@
 // import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 import Player from '@vimeo/player';
 
 // const player = new Player('handstick', {
-//   id: 19231868,
-
-//   width: 640,
+//   // id: 19231868,
+//   // width: 640,
 // });
 
 // player.on('play', function () {
 //   console.log('played the video!');
 // });
+// player.setVolume(0.5);
+// //
 
 // const onPlay = function (data) {
-//   // data is an object containing properties specific to that event
+//   player.setCurrentTime(time);
+//   console.log(player);
 // };
 
 // player.on('play', onPlay);
+// let time = 0;
+// const timeupdate = function (data) {
+//   time = data.seconds;
+//   console.log(time);
+// };
+
+// player.on('timeupdate', timeupdate);
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('play', function (event) {
-  console.log('played the video!');
-  console.log(player);
-});
+// player.on('play', function (event) {
+//   console.log('played the video!');
+//   console.log(player);
+// });
 
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
+// player.getVideoTitle().then(function (title) {
+//   console.log('title:', title);
+// });
 
 player.setVolume(0.5);
 
-console.log(player);
-
-const onPlay = function (data) {
-  player.setCurrentTime(time);
-};
-
+const onPlay = function (data) {};
+const currentTime = JSON.parse(
+  localStorage.getItem('videoplayer-current-time')
+);
+player.setCurrentTime(currentTime);
 player.on('play', onPlay);
 let time = 0;
 const timeupdate = function (data) {
   time = data.seconds;
-  console.log(time);
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(time));
 };
 
-player.on('timeupdate', timeupdate);
+player.on('timeupdate', throttle(timeupdate, 1000));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
+const input = document.querySelector('.feedback-form input');
+const textArea = document.querySelector('.feedback-form textarea');
+const form = document.querySelector('.feedback-form');
 
+const STORAGE_KEY = 'feedback-form-state';
+form.addEventListener('submit', onFormSubmit);
+
+populateForm();
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  console.log(data);
+
+  event.currentTarget.reset();
+
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+const data = {};
+form.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormInput(event) {
+  data[event.target.name] = event.target.value;
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
+}
+
+function populateForm(event) {
+  const storage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (storage) {
+    console.log(storage);
+    input.value = storage.email;
+    textArea.value = storage.message;
+  }
+}
